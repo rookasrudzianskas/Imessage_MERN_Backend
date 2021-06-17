1cf041aeb21406828701e39c28659bf71a31d5d8
Rokas <rookas.rudzenskas@gmail.com>
2021-06-17 14:31:28 +0300

    Roaks

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 // imports
-
+import express from "express";
+import mongoose from "mongoose";
+import Pusher from "pusher";
+import cors from "cors";
 
 // app config
-
+const app = express();
+const port = process.env.PORT || 9001;
 
 
 // middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+// db config
+
+
+// api routes
+
+app.get('/', ((req, res) => res.status(200).send("Backend is working on 🚀")));
+
+// listen
+
+app.listen(port, () => console.log(`listening on port ${port}`));
